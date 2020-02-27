0f5ef36e10d3519919124f75ef2b0337ef0aa1d6
spiceywolf <SpiceyWolf.ArchaicSoft@outlook.com>
2020-02-27 18:15:43 -0500

    Update build scripts to auto-run build on the frontend dependancies

--- script_build.bat
+++ script_build.bat
@@ -1,3 +1,15 @@
+:: Go build the submodules first
+cd .\subprojects\m64p-frontend-cli\project
+call qmake
+call make
+cd ..\..\m64p-frontend-gui\project
+call qmake
+call make
+cd ..\..\..
+
+:: Build mupen64plus.node
+npm install
+
 call npm install
 
 :: Keep console open when script finishes
--- script_build.sh
+++ script_build.sh
@@ -1,3 +1,13 @@
+# Go build the submodules first
+cd ./subprojects/m64p-frontend-cli/project
+qmake
+make
+cd ../../m64p-frontend-gui/project
+qmake
+make
+cd ../../..
+
+# Build mupen64plus.node
 npm install
 
 # Keep console open when script finishes
